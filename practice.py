6caa5908ba7555e35ef4e730a78fe30632767f1c
Lee Youngjun <zerojunee@naver.com>
2021-06-21 15:57:32 +0900

    python study

--- practice.py
+++ practice.py
@@ -201,7 +201,7 @@ print(java)
 
 ### 자료구조의 변경
 menu = {"커피", "우유", "주스"}
-print(menu, type(menu))
+print(menu, type(menu)) 
 
 menu = list(menu)
 print(menu, type(menu))
--- quiz.py
+++ quiz.py
@@ -5,7 +5,7 @@ date = randint(4, 28)
 print("오프라인 스터디 모임 날짜는 매월" + str(date) + "일로 선정되었습니다")
 
 ## 싸이트별 랜덤 비밀번호 생성하기
-url = "http://naver.com"
+url = "http://naver.com" 
 my_str = url.replace("http://", "")
 my_str = my_str[:my_str.index(".")] # my_str[0:5]와 같다.
 
